effd9c6ab8dd103da54487fdf2b736e03b4fa2fd
xuzh0013 <xuzh0013@e.ntu.edu.sg>
2015-10-15 18:33:49 +0800

    sam commit

---
+++ familyFinancial/src/com/hi5/ff/entity/Profile.java
@@ -0,0 +1,35 @@
+package com.hi5.ff.entity;
+
+public class Profile {
+
+	int profileId;
+	String profileName;
+	String profileDesc;
+	public int getProfileId() {
+		return profileId;
+	}
+
+	public void setProfileId(int profileId) {
+		this.profileId = profileId;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getProfileDesc() {
+		return profileDesc;
+	}
+
+	public void setProfileDesc(String profileDesc) {
+		this.profileDesc = profileDesc;
+	}
+
+
+
+
+}
